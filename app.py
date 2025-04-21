893a61a120b1c55ed8f907385a4f585d74b1a61e
amandabarth <112357592+amandabarth@users.noreply.github.com>
2025-04-21 16:16:40 -0400

    added all pages to flask

--- app.py
+++ app.py
@@ -5,8 +5,25 @@ import pandas as pd
 app = flask.Flask(__name__)
 
 @app.route("/")
-def home():
-    return flask.render_template("home.html")
+def base():
+    return flask.render_template("base.html")
+
+@app.route("/browse")
+def browse():
+    return flask.render_template("browse.html")
+
+@app.route("/search")
+def search():
+    return flask.render_template("search.html")
+
+@app.route("/stats")
+def stats():
+    return flask.render_template("stats.html")
+
+@app.route("/user_fav")
+def user_fav():
+    return flask.render_template("user_fav.html")
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
